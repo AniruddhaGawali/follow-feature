0e133faf5a683feb499668953328bedf7c3e64e2
Aniruddha Gawali <aniruddhagawali05@gmail.com>
2024-06-01 15:16:37 +0530

    chore: Update user card component with UI improvements

--- src/app/[user]/page.tsx
+++ src/app/[user]/page.tsx
@@ -3,6 +3,15 @@ import { UserCard } from '@/components/user-card';
 import * as React from 'react';
 import { useState } from 'react';
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { LoaderCircle } from 'lucide-react';
+
 export interface IUserDetailProps {
   params: { user: string };
 }
@@ -24,32 +33,46 @@ export default function UserDetail({ params }: IUserDetailProps) {
       });
   }, [userId]);
 
+  if (!user || !followers || !following) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <LoaderCircle className="w-10 h-10 text-primary-500 animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen  m-10">
-      <div className="flex flex-col items-center justify-center border-2 border-black p-8 rounded-lg hover:shadow-lg cursor-pointer">
-        <h1 className="text-3xl font-bold mb-4">{user?.name}</h1>
+      <Card className="min-w-[250px] text-center">
+        <CardHeader>
+          <CardTitle>{user.name}</CardTitle>
+          <CardDescription>{user.email}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Followers: {followers.length}</p>
+          <p>Following: {following.length}</p>
+        </CardContent>
+      </Card>
 
-        <h2 className="text-lg font-semibold text-gray mb-4">{user?.email}</h2>
-
-        <p
-          className={`${
-            followers.length > 0 ? 'text-green-500' : 'text-red-500'
-          } text-base`}
-        >
-          Followers: {followers.length}
-        </p>
-
-        <p
-          className={`${
-            following.length > 0 ? 'text-green-500' : 'text-red-500'
-          } text-base`}
-        >
-          Following: {following.length}
-        </p>
-      </div>
-      ¸
+      <h2 className="text-2xl font-bold text-center mt-10">Follower</h2>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-5">
         {followers.map((follower) => (
+          <UserCard
+            key={follower.id}
+            user={{
+              id: follower.id,
+              email: follower.email,
+              followers: follower.followers,
+              following: follower.following,
+              name: follower.name,
+            }}
+          />
+        ))}
+      </section>
+
+      <h2 className="text-2xl font-bold text-center mt-10">Following</h2>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-5">
+        {following.map((follower) => (
           <UserCard
             key={follower.id}
             user={{
--- src/components/user-card.tsx
+++ src/components/user-card.tsx
@@ -1,5 +1,14 @@
 import Link from 'next/link';
 import * as React from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Button } from './ui/button';
 
 export interface IUserCardProps {
   user: User;
@@ -8,29 +17,19 @@ export interface IUserCardProps {
 export function UserCard(props: IUserCardProps) {
   return (
     <Link href={`/${props.user.id}`}>
-      <div className="flex flex-col items-center justify-center border-2 border-black p-8 rounded-lg hover:shadow-lg">
-        <h1 className="text-3xl font-bold mb-4">{props.user.name}</h1>
-
-        <h2 className="text-lg font-semibold text-gray mb-4">
-          {props.user.email}
-        </h2>
-
-        <p
-          className={`${
-            props.user.followers.length > 0 ? 'text-green-500' : 'text-red-500'
-          } text-base`}
-        >
-          Followers: {props.user.followers.length}
-        </p>
-
-        <p
-          className={`${
-            props.user.following.length > 0 ? 'text-green-500' : 'text-red-500'
-          } text-base`}
-        >
-          Following: {props.user.following.length}
-        </p>
-      </div>
+      <Card className='min-w-[250px] text-center'>
+        <CardHeader>
+          <CardTitle>{props.user.name}</CardTitle>
+          <CardDescription>{props.user.email}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Followers: {props.user.followers.length}</p>
+          <p>Following: {props.user.following.length}</p>
+        </CardContent>
+        <CardFooter className='items-center'>
+          <Button className='m-auto'>View Profile</Button>
+        </CardFooter>
+      </Card>
     </Link>
   );
 }
